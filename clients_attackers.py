e646d59466a165e0bd4b0b7c7ddc29d52842fbca
Priyesh Ranjan <pr8pf@umsystem.edu>
2024-09-28 19:50:46 -0500

    update

--- clients_attackers.py
+++ clients_attackers.py
@@ -26,7 +26,7 @@ class Attacker_LF(Client):
             print(self.flip.keys())
             print(target)
             print(self.flip['8'])
-            target_ = torch.tensor(list(map(lambda x: int(self.flip[str(x)]) if (str(x) in self.flip.keys() and random() <= self.PDR) else x, target)))
+            target_ = torch.tensor(list(map(lambda x: int(self.flip[str(x)]) if (str(x) in self.flip.keys()) else x, target)))
             #target_ = torch.tensor(list(map(lambda x: 3 if (x in [8] and random() <= self.PDR) else x, target)))
             assert target.shape == target_.shape, "Inconsistent target shape"
             print(target)
