cd3f1795e275d1fffb9446cb9a9fd1d12a795e30
Priyesh Ranjan <pr8pf@umsystem.edu>
2024-09-28 20:01:20 -0500

    update

--- clients_attackers.py
+++ clients_attackers.py
@@ -25,7 +25,6 @@ class Attacker_LF(Client):
             print(self.flip)
             print(list(self.flip.keys()))
             print(target)
-            print(self.flip['8'])
             target_ = torch.tensor(list(map(lambda x: int(self.flip[str(x)]) if (str(x) in self.flip.keys()) else x, target)))
             print(torch.tensor(list(map(lambda x: True if str(x) in ['8'] else False, target))))
             #target_ = torch.tensor(list(map(lambda x: 3 if (x in [8] and random() <= self.PDR) else x, target)))
