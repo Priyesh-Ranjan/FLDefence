a03d498058cffab68130d75c120b15f792c10835
Priyesh Ranjan <pr8pf@umsystem.edu>
2024-09-28 19:11:40 -0500

    update

--- clients_attackers.py
+++ clients_attackers.py
@@ -22,12 +22,9 @@ class Attacker_LF(Client):
     def data_transform(self, data, target, epoch):
         
         if epoch in self.interval:
-            #target_ = torch.tensor(list(map(lambda x: int(self.flip[str(x)]) if (str(x) in self.flip.keys() and random() <= self.PDR) else x, target)))
-            print(self.flip)
-            target_ = torch.tensor(list(map(lambda x: 3 if (x in [8] and random() <= self.PDR) else x, target)))
+            target_ = torch.tensor(list(map(lambda x: self.flip[x] if (x in self.flip.keys() and random() <= self.PDR) else x, target)))
+            #target_ = torch.tensor(list(map(lambda x: 3 if (x in [8] and random() <= self.PDR) else x, target)))
             assert target.shape == target_.shape, "Inconsistent target shape"
-            print(target)
-            print(target_)
         else : 
             target_ = target
         return data, target_
