95f01c3595074b8d166c1171532b25788b76e31f
Priyesh Ranjan <pr8pf@umsystem.edu>
2024-10-10 21:24:48 -0500

    Update

--- clients_attackers.py
+++ clients_attackers.py
@@ -38,7 +38,7 @@ class Attacker_LF(Client):
         self.model.load_state_dict(deepcopy(newState))    
     
     def return_params(self) :
-        return set([y for x,y in self.flip.items()])
+        return set(list(self.flip.keys()))
 
 class Attacker_BD(Client):
     def __init__(self, cid, PDR, scaling, interval, magnitude, pattern, transform, label, model, dataLoader, optimizer, criterion=F.nll_loss, device='cpu', inner_epochs=1):
